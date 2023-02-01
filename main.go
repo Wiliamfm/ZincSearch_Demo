08207d3b081aeb9d80309cb3ca27332f16ee20b4
William Fonseca <santifon15@gmail.com>
2023-02-01 16:57:36 -0500

    add profiles

--- main.go
+++ main.go
@@ -14,20 +14,11 @@ import (
 
 var cpuprofile = flag.String("cpuprofile", "cpu.prof", "Write cpu profile to file")
 var memprofile = flag.String("memprofile", "mem.prof", "write memory profile to `file`")
+var heapprofile = flag.String("heapprofile", "heap.prof", "write memory profile to `file`")
 
 func main() {
 	flag.Parse()
-	if *cpuprofile != "" {
-		f, err := os.Create(*cpuprofile)
-		if err != nil {
-			log.Fatal("could not create CPU profile: ", err)
-		}
-		defer f.Close() // error handling omitted for example
-		if err := pprof.StartCPUProfile(f); err != nil {
-			log.Fatal("could not start CPU profile: ", err)
-		}
-		defer pprof.StopCPUProfile()
-	}
+	profiles()
 	path := os.Args[1] + "/maildir"
 	//emails := indexer.SetEmails(path)
 	//listEmails := indexer.SetEmailsV2("/home/william/Downloads/enron_mail_test/maildir")
@@ -41,17 +32,6 @@ func main() {
 	if indexer.LoadDataBulkV2(emails, "http://localhost:4080/api/_bulkv2", "admin", "Complexpass#123") {
 		fmt.Println("Data loaded")
 	}
-	if *memprofile != "" {
-		f, err := os.Create(*memprofile)
-		if err != nil {
-			log.Fatal("could not create memory profile: ", err)
-		}
-		defer f.Close() // error handling omitted for example
-		runtime.GC()    // get up-to-date statistics
-		if err := pprof.WriteHeapProfile(f); err != nil {
-			log.Fatal("could not write memory profile: ", err)
-		}
-	}
 }
 
 func printEmails(emails []models.Email) {
@@ -64,5 +44,42 @@ func printEmails(emails []models.Email) {
 				//fmt.Println("Content:\n", file.Content)
 			}
 		}
+	}
+}
+
+func profiles() {
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal("could not create CPU profile: ", err)
+		}
+		defer f.Close() // error handling omitted for example
+		if err := pprof.StartCPUProfile(f); err != nil {
+			log.Fatal("could not start CPU profile: ", err)
+		}
+		defer pprof.StopCPUProfile()
+	}
+
+	if *memprofile != "" {
+		f, err := os.Create(*memprofile)
+		if err != nil {
+			log.Fatal("could not create memory profile: ", err)
+		}
+		defer f.Close() // error handling omitted for example
+		runtime.GC()    // get up-to-date statistics
+		if err := pprof.WriteHeapProfile(f); err != nil {
+			log.Fatal("could not write memory profile: ", err)
+		}
+	}
+
+	if *heapprofile != "" {
+		f, err := os.Create(*heapprofile)
+		if err != nil {
+			log.Fatal("could not create heap profile: ", err)
+		}
+		if err = pprof.Lookup("heap").WriteTo(f, 0); err != nil {
+			log.Fatal("could not start CPU profile: ", err)
+		}
+		defer f.Close() // error handling omitted for example
 	}
 }
